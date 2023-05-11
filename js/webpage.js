8cb6dfab76b28759ba5c59c7ea71b47d11dfb273
Alex <alex.f.perez711@gmail.com>
2023-05-10 23:29:15 -0500

    added gitignore and settings button and email form

--- js/webpage.js
+++ js/webpage.js
@@ -40,15 +40,48 @@ settingsLink.addEventListener('click', showPopup);
 function showPopup() {
     const popup = document.createElement('div');
     popup.classList.add('popup');
-
     popup.innerHTML = `
-        <h2>Settings</h2>
-        <p>This is the settings popup.</p>
-    `;
-
+            <h2>Settings</h2>
+            <p>This is the settings popup.</p>
+            <a href="#" id="closeButton" onclick="hidePopup()">Close</a>
+        `;
     document.body.appendChild(popup);
-
-    setTimeout(() => {
-        popup.remove();
-    }, 10000);
 }
+
+function hidePopup() {
+    const popup = document.querySelector('.popup');
+    if (popup) {
+        popup.remove();
+    }
+}
+
+function sendEmail(event) {
+    event.preventDefault();
+
+    // Initialize EmailJS with your user ID
+    emailjs.init("YOUR_USER_ID");
+
+    // Get the form values
+    const form = document.getElementById("contactForm");
+    const name = form.elements.name.value;
+    const email = form.elements.email.value;
+    const subject = form.elements.subject.value;
+    const message = form.elements.message.value;
+
+    // Send the email
+    emailjs.send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", {
+        from_name: name,
+        from_email: email,
+        subject: subject,
+        message: message
+    })
+        .then(function(response) {
+            console.log("Email sent successfully", response);
+            // You can add your own success message or redirect to another page here
+        }, function(error) {
+            console.error("Email sending failed", error);
+            // You can add your own error message or handling here
+        });
+}
+
+
