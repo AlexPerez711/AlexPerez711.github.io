91d7e5e31264ed431134f277ff944e87015a3158
Alex <alex.f.perez711@gmail.com>
2023-05-14 18:38:52 -0500

    updates

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,30 +1,13 @@
 'use strict';
+
+
 const list = document.querySelectorAll('.list');
 function activeLink() {
     list.forEach((item) => item.classList.remove('active'));
     this.classList.add('active');
 }
 list.forEach((item) => item.addEventListener('click', activeLink));
-function homePage() {
-    window.setTimeout(() => {
-        window.location.hash = 'homePage';
-    }, 600);
-}
-function projectPage() {
-    window.setTimeout(() => {
-        window.location.hash = 'projects';
-    }, 600);
-}
-function aboutPage() {
-    window.setTimeout(() => {
-        window.location.hash = 'aboutPage';
-    }, 600);
-}
-function contactPage() {
-    window.setTimeout(() => {
-        window.location.hash = 'contact';
-    }, 600);
-}
+
 
 const scrollLink = document.querySelectorAll('.linkScroll');
 scrollLink.forEach(link => {
@@ -37,27 +20,24 @@ scrollLink.forEach(link => {
         });
     });
 });
+
+
 // Function to update the navigation indicator based on the current section
-document.addEventListener('DOMContentLoaded', function() {
-    function updateIndicator() {
-        const sections = document.querySelectorAll('section');
-        const indicator = document.querySelector('.indicator');
-        const navLinks = document.querySelectorAll('.linkScroll');
-        sections.forEach((section, index) => {
-            const rect = section.getBoundingClientRect();
-            if (rect.top >= 0 && rect.top <= window.innerHeight) {
-                indicator.style.left = `${(index / sections.length) * 100}%`;
-                // Update the active class for the corresponding navigation link
-                navLinks.forEach((link) => {
-                    link.classList.remove('active');
-                });
-                navLinks[index].classList.add('active');
-            }
-        });
+
+
+const sections = document.querySelectorAll('section');
+function getCurrentSection() {
+    for (const section of sections) {
+        const bounding = section.getBoundingClientRect();
+        if (
+            bounding.left >= 0 &&
+            bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+        ) {
+            return section.id;
+        }
     }
-    // Event listener for the scroll event
-    window.addEventListener('scroll', updateIndicator);
-});
+    return null;
+}
 
 // popup
 const settingsLink = document.querySelector('#settings-link');
